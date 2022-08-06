161c150936e7512ebc871985793c97653b471d9c
Ifegwu <agbanyimdaniel29@gmail.com>
2022-08-06 20:26:09 +0000

    url correction

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-serverless123.azurewebsites.net:$PORT/predict 
+     -X POST https://flask-ml-serverless21.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
